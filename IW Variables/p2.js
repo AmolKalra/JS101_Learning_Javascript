497e58c8cf1642e6e1f22dce2927c4e7048b24c7
AmolKalra <amolkalra94@gmail.com>
2022-09-16 19:01:51 +0530

    Variable IW

---
+++ IW Variables/p2.js
@@ -0,0 +1,16 @@
+// Problem 2
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+let name = "Sunny Kalra";
+console.log(name);
+
+name = "Nirpendra Singh";
+console.log(name);
+
+name = "Manlochan Kaur";
+console.log(name);
---
+++ IW Variables/p4.js
@@ -0,0 +1,16 @@
+// Problem 4
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+
+let name = "Sunny Kalra";
+let school = "NEPS";
+let grade = "A1";
+let section = 'D';
+let rollno = "0622";
+
+
+console.log("🆁 🅴 🅿 🅾 🆁 🆃     🅲 🅰 🆁 🅳");
+
+console.log('\n', "Name", ' ' , "School", ' ', "Grade", ' ', "Section", ' ', "Roll-no")
+
+console.log('\n', name, ' ' , school, ' ', grade, ' ', section, ' ', rollno);
